e22c97ff7ab3cf9234e2eb29a589c307262526d3
Claire Bohlen <clairebohlen@gmail.com>
2020-12-02 14:54:51 -0700

    modal cleanup and comments before merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import { BrowserRouter as Router } from "react-router-dom";
+// import Enter from '../src/pages/enter'
+
 
 function App() {
   return (
     <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path={["/", "/books"]}>
-            <Books />
-          </Route>
-          <Route exact path="/books/:id">
-            <Detail />
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
+       
     </Router>
   );
 }
