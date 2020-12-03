36dd56110139370bf35a18cf4e409d623dacb031
Tessa Pierce <tesssapierce@gmail.com>
2020-12-03 13:42:43 -0700

    API Routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+const shakeItRoutes = require("./shakeIt");
 
 // Book routes
-router.use("/books", bookRoutes);
+router.use("/shakeIt", shakeItRoutes);
 
 module.exports = router;
