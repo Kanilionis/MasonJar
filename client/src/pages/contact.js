ecfd6d35a356e4f60e3087eeaf143360cb1cd08b
Claire Bohlen <clairebohlen@gmail.com>
2020-12-03 14:04:07 -0700

    style

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Nav from "../components/Nav";
+import "../index.css"
 
 class Contact extends React.Component {
   
@@ -36,33 +37,39 @@ class Contact extends React.Component {
   render() {
     return(
       <>
-      <Nav />
-      <div className ="row">
-
-    <div className="container col-4 home-container">
-    <h1>Make A Suggestion</h1>
-      <p>Didn't see something you like? Make a suggestion to the Shaker Team! </p>
-      <br></br>
-
       
-      <div className="App">
-        <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-          <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <input type="text" className="form-control" id="name" value={this.state.name} onChange={this.onNameChange.bind(this)} />
+      <div class="contact-container">
+      <Nav />
+        <div className ="row suggestion-row">
+        <div className="container col-4 home-container"></div>
+        
+          <div className="container col-4 home-container signup">
+            <div className="form-container-backdrop">
+              <h1 className = "h1-contact">Make A Suggestion</h1>
+                <p className="p-contact">Didn't see something you like? Make a suggestion to the Shaker Team!</p>
+                  <br></br>
+
+              
+              <div className="App">
+                <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+                  <div className="form-group">
+                      <label htmlFor="name">Name</label>
+                      <input type="text" className="form-control" id="name" value={this.state.name} onChange={this.onNameChange.bind(this)} />
+                  </div>
+                  <div className="form-group">
+                      <label htmlFor="exampleInputEmail1">Email address</label>
+                      <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
+                  </div>
+                  <div className="form-group">
+                      <label htmlFor="message">Suggestion</label>
+                      <textarea className="form-control" rows="5" id="message" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
+                  </div>
+                  <button type="submit" className="btn btn-primary button-contact-form">Submit</button>
+                </form>
+              </div>
+            </div>
           </div>
-          <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Email address</label>
-              <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
-          </div>
-          <div className="form-group">
-              <label htmlFor="message">Suggestion</label>
-              <textarea className="form-control" rows="5" id="message" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-      </div>
-      </div>
+         </div>
       </div>
       </>
     );
