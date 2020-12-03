3c97d810419a52229902f6624f607ba8a94d6087
Tessa Pierce <tesssapierce@gmail.com>
2020-12-03 13:45:53 -0700

    howdy

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,10 +3,10 @@ import axios from "axios";
 export default {
   // Gets all themes
   getThemes: function() {
-    return axios.get("/api/shakeIt/themes");
+    return axios.get("/api/shakeit/themes");
   },
   // gets all activities by a given theme
   getActivitiesByTheme: function(theme) {
-    return axios.get("/api/shakeIt/" + theme);
+    return axios.get("/api/shakeit/" + theme);
   }
 };
