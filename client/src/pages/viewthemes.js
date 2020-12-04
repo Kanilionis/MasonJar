a9a18774f7370027cbff6b6a1aca96e49f03ea3f
Claire Bohlen <clairebohlen@gmail.com>
2020-12-04 11:14:17 -0700

    adding route in app JS

--- client/src/pages/viewthemes.js
+++ client/src/pages/viewthemes.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import TabsPage from '../components/Tabs'
 
 const viewthemes = () => {
     return (
         <div>
+
+            <Nav/>
+
+            <TabsPage/>
             
         </div>
     )
