ddde743de9d7ac11ccfae573a3876655a2e4b9ce
Tessa Pierce <tesssapierce@gmail.com>
2020-12-04 15:55:25 -0700

    enter page updates

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { useAuth0 } from "../react-auth0-spa";
+import Button from 'react-bootstrap/Button';
 
 const LoginButton = () => {
   const { loginWithRedirect, logout } = useAuth0();
 
   return(
       <>
-       <button onClick={() => loginWithRedirect()}>Log In</button>
-       <button onClick={() => logout({ returnTo: window.location.origin })}>
-       Log Out
-     </button>
+       <Button className="enterBtn" variant="outline-secondary" onClick={() => loginWithRedirect()}>Log In</Button>
+       <Button className="enterBtn" variant="outline-secondary" onClick={() => logout({ returnTo: window.location.origin })}>Log Out</Button>
     </>
   )
 };
