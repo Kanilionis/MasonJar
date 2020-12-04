1934c640edfea7b0be58d90c59555018be8d6772
noracurcio <noracurcio@gmail.com>
2020-12-04 14:27:13 -0700

    login.

---
+++ client/src/components/Login.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useAuth0 } from "../react-auth0-spa";
+
+const LoginButton = () => {
+  const { loginWithRedirect, logout } = useAuth0();
+
+  return(
+      <>
+       <button onClick={() => loginWithRedirect()}>Log In</button>
+       <button onClick={() => logout({ returnTo: window.location.origin })}>
+       Log Out
+     </button>
+    </>
+  )
+};
+
+export default LoginButton;
