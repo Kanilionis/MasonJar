0988a6d578d71e4f6f8f82df0b79eed6ec47ad5f
Claire Bohlen <clairebohlen@gmail.com>
2020-12-07 12:17:42 -0700

    comp finished and merging with tessa

--- client/src/components/EditableText/EditableText.js
+++ client/src/components/EditableText/EditableText.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 const EditableText = (props) => {
     const inputRef = useRef(null);
     const [inputVisible, setInputVisible] = useState(false);
     const [text, setText] = useState(props.text);
 
-    function onClickOutside(e){
+    function onClickOutSide (e){
         //checks if the user is clicking outside of the input
         if (inputRef.current && !inputRef.current.contains(e.target)){
             setInputVisible(false); //Disable text input 
