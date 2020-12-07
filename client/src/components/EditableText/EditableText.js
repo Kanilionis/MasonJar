e587dac3eb37539c3a83f66ac4108586912579be
Claire Bohlen <clairebohlen@gmail.com>
2020-12-07 12:02:43 -0700

    adding state

---
+++ client/src/components/EditableText/EditableText.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const EditableText = (props) => {
+    const inputRef = useRef(null);
+    const [inputVisible, setInputVisible] = useState(false);
+    const [text, setText] = useState(props.text);
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default EditableText
