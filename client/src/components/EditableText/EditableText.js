8b537eff3351e0449cdfac414a554f126da20418
Claire Bohlen <clairebohlen@gmail.com>
2020-12-07 12:05:47 -0700

    adding function to tell if the user clicks outside of the text element

--- client/src/components/EditableText/EditableText.js
+++ client/src/components/EditableText/EditableText.js
@@ -5,6 +5,13 @@ const EditableText = (props) => {
     const [inputVisible, setInputVisible] = useState(false);
     const [text, setText] = useState(props.text);
 
+    function onClickOutside(e){
+        //checks if the user is clicking outside of the input
+        if (inputRef.current && !inputRef.current.contains(e.target)){
+            setInputVisible(false); //Disable text input 
+        }
+    }
+
 
     return (
         <div>
