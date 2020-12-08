3f821b39a418f7ab31e4a0ba175c1cfbd52a9f06
Kanilionis <kanilionis@gmail.com>
2020-12-08 10:55:39 -0700

    font, onclick on nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -10,20 +10,23 @@ import Authenticated from "../components/Authenticated";
 function Nav() {
 
   const { isAuthenticated } = useAuth0();
-
+  function enterSite(){
+    window.location = "/shaker"
+  }
   return (
     <>
     {isAuthenticated ? (
     <RBS.Navbar className="navStyle" bg="transparent" expand="lg">
-
-      <img className="imgNav" src={logo} alt="Shaker Mason Jar"/>
-      <RBS.Nav.Link as={Link} to="/shaker"> <span className="mainNav">Shake It Up</span></RBS.Nav.Link> 
+      <div>
+      <img className="imgNav" src={logo} onClick={enterSite} alt="Shaker Mason Jar"/>
+      </div>
+      {/* <RBS.Nav.Link as={Link} to="/shaker"> <span className="mainNav">Shake It Up</span></RBS.Nav.Link>  */}
       <RBS.Navbar.Collapse id="basic-navbar-nav">
         <RBS.Nav className="mr-auto">
           <RBS.Nav>
-            <RBS.Nav.Link as={Link} to="/contact">Contact</RBS.Nav.Link>
-            <RBS.Nav.Link as={Link} to="/viewthemes">Themes</RBS.Nav.Link> 
-            <RBS.Nav.Link as={Link} to="/profile">Profile</RBS.Nav.Link>            
+            <RBS.Nav.Link as={Link} to="/contact">contact</RBS.Nav.Link>
+            <RBS.Nav.Link as={Link} to="/viewthemes">themes</RBS.Nav.Link> 
+            <RBS.Nav.Link as={Link} to="/profile">profile</RBS.Nav.Link>            
           </RBS.Nav>
         </RBS.Nav>
       </RBS.Navbar.Collapse>
@@ -36,13 +39,13 @@ function Nav() {
 
     <RBS.Navbar className="navStyle" bg="transparent" expand="lg">
 
-      <img className="imgNav" src={logo} alt="Shaker Mason Jar"/>
-      <RBS.Nav.Link as={Link} to="/shaker"> <span className="mainNav">Shake It Up</span></RBS.Nav.Link> 
+      <img className="imgNav" onClick={enterSite} src={logo} alt="Shaker Mason Jar"/>
+      {/* <RBS.Nav.Link as={Link} to="/shaker"> <span className="mainNav">Shake It Up</span></RBS.Nav.Link>  */}
       <RBS.Navbar.Collapse id="basic-navbar-nav">
         <RBS.Nav className="mr-auto">
           <RBS.Nav>
-            <RBS.Nav.Link as={Link} to="/contact">Contact</RBS.Nav.Link>
-            <RBS.Nav.Link as={Link} to="/viewthemes">Themes</RBS.Nav.Link> 
+            <RBS.Nav.Link as={Link} to="/contact">contact</RBS.Nav.Link>
+            <RBS.Nav.Link as={Link} to="/viewthemes">themes</RBS.Nav.Link> 
           </RBS.Nav>
         </RBS.Nav>
       </RBS.Navbar.Collapse>
